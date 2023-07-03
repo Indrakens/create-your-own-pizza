7f6ec22dd278d24a92f3aa8e28085c417bac393c
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-07-03 21:12:04 +0000

    Add get users name

--- run.py
+++ run.py
@@ -5,10 +5,25 @@ def header():
     print("WELCOME TO CREATE YOUR OWN PIZZA!\n")
 
 
+def name():
+    """
+    Get the name of the user.
+    """
+    print("Please enter your name before continuing.")
+    while True:
+        name = input("ENTER YOUR NAME: ")
+        if name.isalpha():
+            print("Hello," + name + "! Enjoy creating your own pizza!\n")
+            return name
+        else:
+            print("Invalid name, please try again!\n")    
+
+
 def main():
     """
     Runs all functions.
     """
     header()
+    name()
 
 main()
